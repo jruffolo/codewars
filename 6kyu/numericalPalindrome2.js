4672cb463e9afac080a6cd53372a739234103c76
Joshua Ruffolo <joshua.ruffolo@gmail.com>
2022-09-20 10:18:26 -0500

    2022-09-20 commit of the day

---
+++ 6kyu/numericalPalindrome2.js
@@ -0,0 +1,28 @@
+function palindrome(num) {
+  if (typeof num !== 'number' || num < 0 || num % 1 !== 0) return 'Not valid';
+  let numStr = String(num);
+  
+  for (let i = 2; i <= numStr.length; i++) {
+    for (let j = 0; j <= numStr.length - i; j++) {
+      if (isPalindrome(numStr.slice(j, j + i))) {
+        return true;
+      }
+    }
+  }
+  
+  return false;
+}
+
+function isPalindrome(str) {
+  let l = 0;
+  let r = str.length - 1;
+  while (l <= r) {
+    if (str[l] !== str[r]) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+  
+  return true;
+}
